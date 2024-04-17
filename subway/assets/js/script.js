51cd2306ad48e25ec93959639391316d6092aca3
dnsgk456 <dnsgk456@gmail.com>
2024-04-17 18:18:35 +0900

    singleProject 2일차

--- subway/assets/js/script.js
+++ subway/assets/js/script.js
@@ -10,13 +10,27 @@
         }
     });
 
-    let menuSwiper = new Swiper(`.section-menu .menu-swiper`, {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        touchRatio: 0,
-        navigation: {
-            nextEl: '.menu-swiper .swiper-button-next',
-            prevEl: '.menu-swiper .swiper-button-prev'
-        }
-    })
+    const menuTabs = document.querySelectorAll(`.section-menu .menu-tabs li`);
+    const menuSwipers = document.querySelectorAll(`.section-menu .menu-swiper`);
+    menuTabs.forEach((tab, idx)=> {
+        menuTabs[idx].addEventListener(`click`, () =>{
+            menuTabs.forEach(t => {t.classList.remove(`active`);});
+            menuSwipers.forEach(s => { s.classList.remove(`active`);});
+            menuTabs[idx].classList.add(`active`);
+            menuSwipers[idx].classList.add(`active`);
+        });
+    });
+
+    menuSwipers.forEach(swiper => {
+        const menuSwiper = new Swiper(swiper.querySelector(`.swiper`), {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 20,
+            touchRatio: 0,
+            navigation: {
+                nextEl: swiper.querySelector('.swiper-button-next'),
+                prevEl: swiper.querySelector('.swiper-button-prev')
+            }
+        });
+    });
 })();
